3db954b28d8da3487c67b643e1a462352c7f60ec
Jonathan Lozinski <jonathan.lozinski@gmail.com>
2013-10-02 22:44:09 +0100

    Playing Card and deck models

---
+++ Matchismo/Models/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by Jonathan Lozinski on 02/10/2013.
+//  Copyright (c) 2013 Jonathan Lozinski. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/Models/PlayingCardDeck.m
@@ -0,0 +1,27 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by Jonathan Lozinski on 02/10/2013.
+//  Copyright (c) 2013 Jonathan Lozinski. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+-(id)init {
+    self = [super init];
+    if (self) {
+        for(NSString *suit in [PlayingCard validSuits]) {
+            for(NSUInteger rank=1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card atTop:YES];
+            }
+        }
+    }
+    return self;
+}
+@end
